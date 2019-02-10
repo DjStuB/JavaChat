a3f7a6c7932b3730cb770627386323f0f8ec5b26
DjStuB <kolya.shimarov2@yandex.ru>
2019-02-10 20:48:13 +0400

    First step

---
+++ src/main/java/com/simbirsoft/Shimarovinternship/ShimarovInternshipApplication.java
@@ -0,0 +1,14 @@
+package com.simbirsoft.Shimarovinternship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShimarovInternshipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShimarovInternshipApplication.class, args);
+	}
+
+}
+
