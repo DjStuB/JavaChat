549569efb8e5e80c18f48ede376d644d3420f71f
DjStuB <kolya.shimarov2@yandex.ru>
2019-02-11 00:19:25 +0400

    Putting image

--- src/main/java/com/simbirsoft/Shimarovinternship/ShimarovInternshipApplication.java
+++ src/main/java/com/simbirsoft/Shimarovinternship/ShimarovInternshipApplication.java
@@ -3,11 +3,15 @@ package com.simbirsoft.Shimarovinternship;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.swing.*;
+import java.net.URL;
+
 @SpringBootApplication
 public class ShimarovInternshipApplication {
 public static void main(String[] args) {
 SpringApplication.run(ShimarovInternshipApplication.class, args);
-}
+URL iconURL = ClassLoader.getSystemResource("/images/icon.png");
+ImageIcon imageIcon = new ImageIcon(iconURL);}
 
 }
 
